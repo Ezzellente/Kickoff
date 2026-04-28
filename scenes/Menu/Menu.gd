4eb25d31124f8dca3b70caf0ea6d2d551fbd0caf
leandro sechuk <leandrosechuk@gmail.com>
2026-04-28 15:35:03 -0300

    Conectar manualmente (con .connect)

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -1,27 +1,18 @@
 extends Node2D
 
 const INSTANCES_TEXT = "Cantidad de instancias: "
-# Variable para contar cantidad de instancias
 var instancesCount = 0
 
 func _ready():
-	# Inicializa el texto de "CountLabel" usando el texto
-	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
-	pass
+	
+	$RedGenerator.button_down.connect(count_new_instance)
+	$VioletGenerator.button_down.connect(count_new_instance)
 
-func _on_red_generator_button_down() -> void:
-	pass # Replace with function body.
-
-	# Inicializa el texto de "CountLabel"
-	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
-
-# Esta es la función que debías agregar:
 func count_new_instance():
-	# Aumenta en 1 el valor de la variable
 	instancesCount += 1
-	# Actualiza el texto de CountLabel
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 
-func _on_violet_generator_button_down() -> void:
-	pass
+func delete():
+	instancesCount = 0
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
