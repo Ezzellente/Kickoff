0bec77225a70d83a41290e383903000394e883fa
leandro sechuk <leandrosechuk@gmail.com>
2026-04-28 11:30:42 -0300

    Conectar GenerateBlock

---
+++ scenes/Buttons/VioletGenerator/generate_block.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+var block_scene = preload("res://scenes/Blocks/Violet/BlockViolet.tscn")
+
+func _on_violet_generator_button_down() -> void:
+	pass # Replace with function body.
